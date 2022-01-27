10bf677b5899401a31d0bf0002e67b7b6bc383d3
ConsensusAI <malik.iqbal@hotmail.ca>
2022-01-27 04:09:20 +0000

    Implement tail function to return array values started from the second value

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+
+const emptyArray = [];
+const singleValueArray = [1];
+
+assertEqual(tail(emptyArray)[0], undefined);
+assertEqual(tail(singleValueArray)[0], undefined);
