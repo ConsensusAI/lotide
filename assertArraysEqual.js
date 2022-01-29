f1cc40af8bc33de964d1bb13585b43cd11a2cb68
ConsensusAI <malik.iqbal@hotmail.ca>
2022-01-29 05:02:18 +0000

    Implement function printing assertion results for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  let equality = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equality = false;
+    }
+  }
+  return equality;
+}
+
+const assertArraysEqual = function(originalArray, comparedArray) {
+  if (eqArrays(originalArray, comparedArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${originalArray} === ${comparedArray}`)
+  } else if (!eqArrays(originalArray, comparedArray)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${originalArray} !== ${comparedArray}`);
+  }
+}
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
