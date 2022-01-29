71c1c18c99dfe285676175d0f7f34e65669c1f05
ConsensusAI <malik.iqbal@hotmail.ca>
2022-01-29 17:07:17 +0000

    Implement function to return arrays of indices where letters are found in a passed string

---
+++ letterPositions.js
@@ -0,0 +1,54 @@
+const assertEqual = function(actual, expected) {
+  console.log(Array.isArray(actual) + Array.isArray(expected));
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let equality = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equality = false;
+    }
+  }
+  return equality;
+};
+
+const assertArraysEqual = function(originalArray, comparedArray) {
+  if (eqArrays(originalArray, comparedArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${originalArray} === ${comparedArray}`);
+  } else if (!eqArrays(originalArray, comparedArray)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${originalArray} !== ${comparedArray}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  let positions = {};
+  let i = 0;
+  for (letter of sentence) {
+    if (letter === " ") {
+      i++;
+      continue;
+    }
+    if (positions[letter]) {
+      positions[letter].push(i);
+    } else {
+      positions[letter] = [i];
+    }
+    i++;
+    console.log(positions);
+  }
+  return positions;
+};
+
+//Test
+let test = "hello";
+let testResult = letterPositions(test);
+assertArraysEqual(testResult["h"], [0]);
+assertArraysEqual(testResult["e"], [1]);
+assertArraysEqual(testResult["l"], [2, 3]);
+assertArraysEqual(testResult["o"], [4]);
+console.log(testResult["h"]);
