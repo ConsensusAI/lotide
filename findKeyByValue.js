d0d8a789a3135596ad2993d7dc2841f181b246d7
ConsensusAI <malik.iqbal@hotmail.ca>
2022-01-29 18:33:04 +0000

    Implement function to find first key associated with passed value in an object

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let key;
+  for (let item in object) {
+    if (object[item] === value) {
+       key = item;
+    }
+  }
+  return key;
+};
+
+//Test Code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
