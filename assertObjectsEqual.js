5f672eca058452095e6ef7d489e5c97812af496e
ConsensusAI <malik.iqbal@hotmail.ca>
2022-01-30 15:21:40 +0000

    Lint code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -14,7 +14,6 @@ const eqArrays = function(array1, array2) {
 const eqObjects = function(object1, object2) {
   let result = true;
   let keys1 = Object.keys(object1);
-  let keys2 = Object.keys(object2);
   let numberOfKeys1 = Object.keys(object1).length;
   let numberOfKeys2 = Object.keys(object2).length;
   if (numberOfKeys1 !== numberOfKeys2) {
@@ -24,7 +23,7 @@ const eqObjects = function(object1, object2) {
     if (Array.isArray(object1[item]) || Array.isArray(object2[item])) {
       if (!eqArrays(object1[item], object2[item])) {
         return false;
-      } 
+      }
     } else if (object1[item] !== object2[item]) {
       return false;
     }
